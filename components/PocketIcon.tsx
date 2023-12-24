3b51e63704a7c15bea4da8dfabff17c6f7a3c799
Bagas Meyca <bagasmputra@gmail.com>
2023-12-24 19:35:25 +0700

    feat(icon): Prepare Default Icon Array for pocket

---
+++ components/PocketIcon.tsx
@@ -0,0 +1,54 @@
+import { IconName, IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBookOpen,
+  faBorderAll,
+  faCarSide,
+  faCartShopping,
+  faCircle,
+  faFilm,
+  faHouse,
+  faLaptop,
+  faScrewdriverWrench,
+  faSeedling,
+  faShirt,
+  faTicket,
+  faUmbrellaBeach,
+  faUtensils,
+  faWifi,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export type PocketIconType = { name: IconName; element: IconDefinition };
+
+export const Category = [
+  "Groceries",
+  "Needs",
+  "Entertainment",
+  "Personal Development",
+  "Internet",
+  "Subscription",
+  "Transportation",
+  "Annual Spending",
+  "charity",
+  "rent house/cicilan rumah",
+  "Utilities",
+];
+
+export const pocketIconList: PocketIconType[] = [
+  { name: "cart-shopping", element: faCartShopping },
+  { name: "shirt", element: faShirt },
+  { name: "ticket", element: faTicket },
+  { name: "utensils", element: faUtensils },
+  { name: "laptop", element: faLaptop },
+  { name: "book-open", element: faBookOpen },
+  { name: "wifi", element: faWifi },
+  { name: "film", element: faFilm },
+  { name: "car-side", element: faCarSide },
+  { name: "umbrella-beach", element: faUmbrellaBeach },
+  { name: "seedling", element: faSeedling },
+  { name: "house", element: faHouse },
+  { name: "screwdriver-wrench", element: faScrewdriverWrench },
+];
+
+export const getElement = (iconName: string) =>
+  pocketIconList.find((i) => i.name == iconName)?.element ?? faCircle;
